66f016fe3b9b779ca0ff3da0d4d2f66118fa871b
cowilliams_deloitte <cowilliams@deloitte.com>
2024-09-25 13:53:46 -0600

    adding nodemon

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,36 @@
-console.log("heler");
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+// Type definitions (schema)
+const typeDefs = `
+type Query {
+    title: String!,
+    price: Int!,
+    releaseYear: Int!,
+    rating: Float!,
+    inStock: Boolean!,
+}`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    title: () => "Harry Potter",
+    price: () => 20,
+    releaseYear: () => 2001,
+    rating: () => 4.5,
+    inStock: () => true,
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+async function startServer() {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`🚀  Server ready at: ${url}`);
+}
+startServer();

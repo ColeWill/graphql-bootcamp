28ac9cfcc89348b1c9cbfbaf8a9091e6157d4c6a
cowilliams_deloitte <cowilliams@deloitte.com>
2024-10-14 16:45:26 -0600

    working on subscriptions

--- g-04-02-sub-basics/graphql-basics/src/resolvers/Subscription.js
+++ g-04-02-sub-basics/graphql-basics/src/resolvers/Subscription.js
@@ -11,6 +11,17 @@ const Subscription = {
       return pubsub.asyncIterator('count')
     },
   },
+  comment: {
+    subscription(parent, { postId }, { db, pubsub }, info) {
+      const post = db.posts.find((post) => post.id === postId && post.published)
+
+      if (!post) {
+        throw new Error('Post not found')
+      }
+
+      return pubsub.asyncIterator(`comment ${comment.postId}`)
+    },
+  },
 }
 
 export { Subscription as default }

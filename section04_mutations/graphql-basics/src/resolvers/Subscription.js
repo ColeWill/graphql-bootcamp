e1ebc86e19d145a5504aa5b82204b6b303d80c8a
cowilliams_deloitte <cowilliams@deloitte.com>
2024-10-14 15:57:36 -0600

    working on comments subscription

--- section04_mutations/graphql-basics/src/resolvers/Subscription.js
+++ section04_mutations/graphql-basics/src/resolvers/Subscription.js
@@ -1,10 +1,43 @@
 const Subscription = {
   count: {
-    subscribe(parent, args, ctx, info) {
-      let { pubSub } = ctx
-      let count = 0
+    subscribe(parent, args, { pubsub }, info) {
+      if (!pubsub) {
+        throw new Error('pubsub is required!')
+      }
 
-      return pubSub.asynIterator('count')
+      let count = 0
+      const intervalId = setInterval(() => {
+        try {
+          count++
+          pubsub.publish('count', {
+            count,
+          })
+        } catch (err) {
+          console.error('Error publishing count')
+        }
+      }, 1000)
+
+      const asyncIterator = pubsub.asyncIterator('count')
+
+      asyncIterator.return = () => {
+        clearInterval(intervalId)
+        return { done: true }
+      }
+
+      return asyncIterator
+    },
+  },
+  comment: {
+    subscribe(parent, { postId }, { db, pubsub }, info) {
+      const post = db.posts.find(
+        (post) => post.id === postId && post.pubslished
+      )
+
+      if (!post) {
+        throw new Error('Post not found for subscription')
+      }
+
+      return pubsub.asyncIterator(`comment: ${postId}`)
     },
   },
 }
